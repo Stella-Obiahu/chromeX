109514e70e94c2da3f583556a2bcd84bc156b430
Stella-Obiahu <stellapinky85@gmail.com>
2022-12-08 19:26:57 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-let myLeads = ["mhu.com", "dythr.org","yyhbb.i"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const input = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 input.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
 })
+function renderLeads(){
+    let listitems = " "
+    for(let i = 0; i < myLeads.length; i++) {
+        listitems +=  "<li>" + myLeads[i] + "</li> "
+    }
+    ulEl.innerHTML = listitems
 
-for(let i = 0; i < myLeads.length; i++) {
-     ulEl.innerHTML +=  "<li>" + myLeads[i] + "</li> "
 }
 
