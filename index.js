2fd2b13b497a0a27f954e2007f57decc891360c3
Stella-Obiahu <stellapinky85@gmail.com>
2022-12-09 20:18:12 +1300

    update

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ const ulEl = document.getElementById("ul-el")
 
 input.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    inputEl.value =" "
     renderLeads()
 })
 function renderLeads(){
     let listitems = " "
     for(let i = 0; i < myLeads.length; i++) {
-        listitems +=  "<li>" + myLeads[i] + "</li> "
+        //this is how to add a <a> tag so it can navigate to another page
+        //using target='_blank' makes your page open in another broswer
+        listitems += 
+         `<li>
+            <a target = '_blank' href ='${myLeads[i]}'>
+            ${myLeads[i]}
+            </a>
+        </li>`
     }
     ulEl.innerHTML = listitems
-
 }
 
