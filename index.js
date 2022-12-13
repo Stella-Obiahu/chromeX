2d18d735dce0eae77b84b08842e0c1856ed507a3
Stella-Obiahu <stellapinky85@gmail.com>
2022-12-14 09:48:21 +1300

    chrome

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 let myLeads = []
+let oldleads = []
 const inputEl = document.getElementById("input-el")
 const input = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-input.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    inputEl.value =" "
-    localStorage.setItem("myLeads" , JSON.stringify(myLeads))
-    renderLeads()
-})
-function renderLeads(){
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+function render(leads){
     let listitems = " "
-    for(let i = 0; i < myLeads.length; i++) {
+    for(let i = 0; i < leads.length; i++) {
         //this is how to add a <a> tag so it can navigate to another page
         //using target='_blank' makes your page open in another broswer
         listitems += 
          `<li>
-            <a target = '_blank' href ='${myLeads[i]}'>
-            ${myLeads[i]}
+            <a target = '_blank' href ='${leads[i]}'>
+            ${leads[i]}
             </a>
         </li>`
     }
     ulEl.innerHTML = listitems
 }
+deleteBtn.addEventListener("click", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+input.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value =" "
+    localStorage.setItem("myLeads" , JSON.stringify(myLeads))
+    render(myLeads)
+})
+
 
